dc1d7bea4f1f800be498eca82d7809ddaacf6d31
Andrés Zambrano <andres@andreszambrano.com>
2024-02-22 16:11:40 +0100

    Updated script

--- build_script.sh
+++ build_script.sh
@@ -1,8 +1,20 @@
-make clean
-CC=/home/andres/Dev/llvm-project/build/bin/clang
-CXX=/home/andres/Dev/llvm-project/build/bin/clang++
+if [ ! -d "coremark" ]; then
+    git clone https://github.com/eembc/coremark.git
+else
+    cd coremark
+    git fetch origin master
+    git reset --hard origin/master
+    git clean -fdx
+    cd ..
+fi
+LLVM_PROJECT_BIN_PATH=/home/andres/Dev/llvm-project/build/bin
 AARCH_LIB=/usr/lib/gcc/aarch64-linux-gnu/13.2.0/
 SYS_ROOT=/usr/aarch64-linux-gnu
+export CC=$LLVM_PROJECT_BIN_PATH/clang
+CXX=$LLVM_PROJECT_BIN_PATH/clang++
 export CC
 export CXX
-make compile XCFLAGS="-B$AARCH_LIB -L$AARCH_LIB -static --target=aarch64-linux-gnu --sysroot=$SYS_ROOT -fuse-ld=lld"
+make -C coremark compile XCFLAGS="-B$AARCH_LIB -L$AARCH_LIB -static --target=aarch64-linux-gnu --sysroot=$SYS_ROOT -fuse-ld=lld"
+if [ -f "coremark/coremark.exe" ]; then
+    cp coremark/coremark.exe .
+fi
