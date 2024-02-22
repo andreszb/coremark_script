8b8218ca4ba49203306d9c4a5d3188937fb872d5
Andrés Zambrano <andres@andreszambrano.com>
2024-02-22 20:51:55 +0100

    Added script

--- build_script.sh
+++ build_script.sh
@@ -1,3 +1,6 @@
+# Clean previous runs
+git clean -fdx
+# Clone official 'coremark' repository or fetch latest changes/clean previous executions
 if [ ! -d "coremark" ]; then
     git clone https://github.com/eembc/coremark.git
 else
@@ -7,14 +10,17 @@ else
     git clean -fdx
     cd ..
 fi
-LLVM_PROJECT_BIN_PATH=/home/andres/Dev/llvm-project/build/bin
+LLVM_PROJECT_BIN_PATH=/home/andres/Dev/llvm-project/build/bin/
 AARCH_LIB=/usr/lib/gcc/aarch64-linux-gnu/13.2.0/
 SYS_ROOT=/usr/aarch64-linux-gnu
 export CC=$LLVM_PROJECT_BIN_PATH/clang
-CXX=$LLVM_PROJECT_BIN_PATH/clang++
-export CC
-export CXX
+export CXX=$LLVM_PROJECT_BIN_PATH/clang++
 make -C coremark compile XCFLAGS="-B$AARCH_LIB -L$AARCH_LIB -static --target=aarch64-linux-gnu --sysroot=$SYS_ROOT -fuse-ld=lld"
 if [ -f "coremark/coremark.exe" ]; then
-    cp coremark/coremark.exe .
+    mv coremark/coremark.exe ./coremark_nochange
+fi
+make -C coremark clean
+make -C coremark compile XCFLAGS="-B$AARCH_LIB -L$AARCH_LIB -static --target=aarch64-linux-gnu --sysroot=$SYS_ROOT -fuse-ld=lld -mllvm -pa-emu"
+if [ -f "coremark/coremark.exe" ]; then
+    cp coremark/coremark.exe ./coremark_wchange
 fi
